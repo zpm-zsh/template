9195f04c3350c7215c118e73be9653678a24644b
Grigorii Horos <horosgrisa@gmail.com>
2019-08-01 01:38:49 +0300

    First commit

---
+++ templates/React.Class,React Class Component,@1@.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class @1@ extends React.Component {
+  render() {
+    const greeting = 'Hello Function Component!';
+    return <h1>Hello, {greeting}</h1>;
+  }
+}
+
+export default @1@
---
+++ templates/React.Function,React Function Component,@1@.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function @1@() {
+  const greeting = 'Hello Function Component!';
+
+  return <h1>{greeting}</h1>;
+}
+
+export default @1@;

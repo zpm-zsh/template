0e4e65e48f9c098a4fb3f3e2f0db80f7168d6b13
Grigorii Horos <horosgrisa@gmail.com>
2019-08-06 00:11:38 +0300

    Changes to be committed:     renamed:    new-file-from-template.plugin.zsh -> template.plugin.zsh

--- template.plugin.zsh
+++ template.plugin.zsh
@@ -25,63 +25,16 @@ function template(){
     bash "${template_dir}/${template_name},pre.sh" $@
   fi
   
-  if [[ -n $1 ]]; then
-    filename="${filename/_1_/"$1"}"
-  fi
-  
-  if [[ -n $2 ]]; then
-    filename="${filename/_2_/"$2"}"
-  fi
-  
-  if [[ -n $3 ]]; then
-    filename="${filename/_3_/"$3"}"
-  fi
-  
-  if [[ -n $4 ]]; then
-    filename="${filename/_4_/"$4"}"
-  fi
-  
-  if [[ -n $5 ]]; then
-    filename="${filename/_5_/"$5"}"
-  fi
-  
-  if [[ -n $6 ]]; then
-    filename="${filename/_6_/"$6"}"
-  fi
-  
-  if [[ -n $7 ]]; then
-    filename="${filename/_7_/"$7"}"
-  fi
+  for i in `seq 1 ${args_length}`; do
+    filename="${filename/_${i}_/${(P)i}}"
+  done
   
   cp "${filename_orig}" "${filename}"
   
-  if [[ -n $1 ]]; then
-    sed -i "s/__1__/${1}/g" "${filename}"
-  fi
+  for i in `seq 1 ${args_length}`; do
+    sed -i "s/__${i}__/${(P)i}/g" "${filename}"
+  done
   
-  if [[ -n $2 ]]; then
-    sed -i "s/__2__/${2}/g" "${filename}"
-  fi
-  
-  if [[ -n $3 ]]; then
-    sed -i "s/__3__/${3}/g" "${filename}"
-  fi
-  
-  if [[ -n $4 ]]; then
-    sed -i "s/__4__/${4}/g" "${filename}"
-  fi
-  
-  if [[ -n $5 ]]; then
-    sed -i "s/__5__/${5}/g" "${filename}"
-  fi
-  
-  if [[ -n $6 ]]; then
-    sed -i "s/__6__/${6}/g" "${filename}"
-  fi
-  
-  if [[ -n $7 ]]; then
-    sed -i "s/__7__/${7}/g" "${filename}"
-  fi
   
   if [[ -f "${template_dir}/${template_name},post.sh" ]]; then
     bash "${template_dir}/${template_name},post.sh" $@
@@ -90,3 +43,4 @@ function template(){
   echo "${c[cyan]}Created: ${c[yellow]}${filename}"
   
 }
+

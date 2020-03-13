2e754bd93c1ddccec2d26617565423aa982a8442
Grigorii Horos <horosgrisa@gmail.com>
2020-03-13 22:23:13 +0200

    Fix plugin

--- template.plugin.zsh
+++ template.plugin.zsh
@@ -1,33 +1,23 @@
 #!/usr/bin/env zsh
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
-local base_dir=${0:h}
+local _zpm_template__base_dir=${0:h}
 
-function template(){
+function template() {
   emulate -L zsh
   setopt +o nomatch
 
-  local template_folder
-  local template_name
-  local template_description
-  local args_length
-  local file
-  local ___template
-  local name
-  local description
-  local arg_length
-  
   if [[ "$#" -lt 1 ]]; then
     echo "Usage: template template args"
     return 1
   fi
-  
-  template_folder="$(echo ${base_dir}/templates/${1},*,*([1]))"
+
+  local template_folder="$(echo ${_zpm_template__base_dir}/templates/${1},*,*([1]))"
+
   shift
-  template_name="$(basename ${template_folder} | awk -F',' '{print $1}')"
-  template_description="$(basename ${template_folder} | awk -F',' '{print $2}')"
-  args_length="$(basename ${template_folder} | awk -F',' '{print $3}')"
-  
+
+  args_length="$(echo ${___template:t} | cut -d',' -f3 )"
+
   if [[ "$#" -lt $args_length ]]; then
     echo "Need ${args_length} arguments for template"
     if [[ -f "${template_folder}/help.txt" ]]; then
@@ -35,30 +25,30 @@ function template(){
     fi
     return 1
   fi
-  
+
   if [[ -f "${template_folder}/pre.sh" ]]; then
     bash "${template_folder}/pre.sh" $PWD $@
   fi
-  
+
   for file in "${template_folder}/files/"*; do
     if [[ -e "$file" ]]; then
       filename=$(basename $file)
       for i in `seq 1 ${args_length}`; do
         filename="${filename/_${i}_/${(P)i}}"
       done
-      
+
       cp "${file}" "${PWD}/${filename}"
-      
+
       for i in `seq 1 ${args_length}`; do
         sed -i "s/__${i}__/${(P)i}/g" "${PWD}/${filename}"
       done
     fi
   done
-  
+
   if [[ -f "${template_folder}/post.sh" ]]; then
     bash "${template_folder}/post.sh" $PWD $@
   fi
-  
+
   echo "${c[cyan]}Done"
 }
 
@@ -71,10 +61,10 @@ for ___template in ${0:h}/templates/*,*,*; do
   _templates_list+="${name}:${description}, ${arg_length} arg(s)"
 done
 
-_template(){
+function _template() {
   _arguments \
-  '*:: :->subcmds' && return 0
-  
+    '*:: :->subcmds' && return 0
+
   if (( CURRENT == 1 )); then
     _describe -t commands 'Templates' _templates_list
     return

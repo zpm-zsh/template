12dd528fadbf8f290c59f412bf17b38ef2e16854
Grigorii Horos <horosgrisa@gmail.com>
2020-03-15 01:35:00 +0200

    Optimize plugin

--- template.plugin.zsh
+++ template.plugin.zsh
@@ -3,56 +3,11 @@
 0="${${(M)0:#/*}:-$PWD/$0}"
 typeset -g _zpm_template__base_dir=${0:h}
 
-function template() {
-  emulate -L zsh
-  setopt +o nomatch
-
-  if [[ "$#" -lt 1 ]]; then
-    echo "Usage: template template args"
-    return 1
-  fi
-
-  local template_folder="$(echo ${_zpm_template__base_dir}/templates/${1},*,*([1]))"
-
-  shift
-
-  arr_mod=("${(@s/,/)___template:t}")
-
-  args_length="${arr_mod[3]}"
-
-  if [[ "$#" -lt $args_length ]]; then
-    echo "Need ${args_length} arguments for template"
-    if [[ -f "${template_folder}/help.txt" ]]; then
-      cat "${template_folder}/help.txt"
-    fi
-    return 1
-  fi
-
-  if [[ -f "${template_folder}/pre.sh" ]]; then
-    bash "${template_folder}/pre.sh" $PWD $@
-  fi
-
-  for file in "${template_folder}/files/"*; do
-    if [[ -e "$file" ]]; then
-      filename=$(basename $file)
-      for i in `seq 1 ${args_length}`; do
-        filename="${filename/_${i}_/${(P)i}}"
-      done
-
-      cp "${file}" "${PWD}/${filename}"
-
-      for i in `seq 1 ${args_length}`; do
-        sed -i "s/__${i}__/${(P)i}/g" "${PWD}/${filename}"
-      done
-    fi
-  done
-
-  if [[ -f "${template_folder}/post.sh" ]]; then
-    bash "${template_folder}/post.sh" $PWD $@
-  fi
-
-  echo "${c[cyan]}Done"
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
 }
+
+autoload -Uz template
 
 _templates_list=()
 for ___template in ${0:h}/templates/*,*,*; do
